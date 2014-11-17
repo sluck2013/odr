7437cbbeedc17c530a2e4a8c66eb08a594fc8613
Taurus1991 <panbh0427@gmail.com>
2014-11-17 00:16:16 -0500

    mkstemp

--- client.c
+++ client.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(int argc, char** argv) {
+int main(int argc, char** argv) {
     // create domain datagram socket
-    
+	int sockfd;
+	struct sockaddr_un cliaddr servaddr;
+
+	sockfd=Socket(AF_LOCAL, SOCK_DGRAM, 0);
+	char file[]="template.XXXXXX";
+	// create a temp file
+	int tmpFile=mkstemp(file);
+	if(tmpFile==-1) {
+		printf("create temp file failed /n");
+		exit(1);
+	}
+
+	unlink(file);
+
+	bzero(&cliaddr, sizeof(cliaddr) );          //  bind an address for us
+	cliaddr.sun_family=AF_LOCAL;
+	strcpy(cliaddr.sun_path, tmpFile );
+
+	Bind(sockfd, (SA*) &cliaddr, sizeof(cliaddr) );
+
+
     while (1) {
         int iVmNum = 0;
         printf("Please select a VM by typing number 1-10, ");

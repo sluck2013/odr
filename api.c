32953313d12b3f02642e512ee3ec876b31c98058
Taurus1991 <panbh0427@gmail.com>
2014-11-17 20:25:28 -0500

    null

--- api.c
+++ api.c
@@ -1,7 +1,8 @@
 #include "api.h"
 
 void msg_send(int sockfd, char* destIP, int destPort, char* msg, int flag) {
+
 }
 
-void msg_recv(int sockfd, char* msgBuf, char* srcIP, int srcPort) {
+void msg_recv(int sockfd, char* msgBuf, char* srcIP, int* srcPort) {
 }

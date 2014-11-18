173c0f152e141177009600ba74150679f00aa867
Taurus1991 <panbh0427@gmail.com>
2014-11-18 17:39:38 -0500

    api.c

--- api.c
+++ api.c
@@ -17,5 +17,14 @@ void msg_send(int sockfd, char* destIP, int destPort, char* msg, int flag) {
 
 
 void msg_recv(int sockfd, char* msgBuf, char* srcIP, int* srcPort) {
+	// char* packAppData(char* data,const char* destIP, const int destPort, const char* msg, const int flag) 
+	// void unpackAppData(char* data, char* srcIP, int* srcPort, char* msg) 
+	char data[MAXLINE];
+	unpackAppData(data, srcIP, srcPort, msgBuf);
+	struct sockaddr_un suCliAddr;
+    	bzero(&suCliAddr, sizeof(suCliAddr));
+    	suCliAddr.sun_family = AF_LOCAL;
+    	strcpy(suCliAddr.sun_path, ODR_WK_PATH);
+	Recvfrom(sockfd, data, sizeof(data), 0, (SA*) &suCliAddr, sizeof(suCliAddr) );
 }
 
